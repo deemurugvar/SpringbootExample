f5403b870fcc460918bef563361471cbe56d78ec
deemurugvar <deepikamurugan30@gmail.com>
2025-10-04 21:58:54 +0530

    jwt authentication configurations

---
+++ src/main/java/com/deepika/SpringbootExample/utils/JWTUtils.java
@@ -0,0 +1,10 @@
+package com.deepika.SpringbootExample.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtils {
+    private final String SECRET= "";
+    private final long EXPIRATION= 1000*30;
+
+}
